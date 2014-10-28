900270c5c605d2c4626615de5597dfbb98a00904
861645007 <861645007@qq.com>
2014-10-28 19:19:50 +0800

    Initial Commit

---
+++ RememberRoad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RememberRoad
+//
+//  Created by wanghuanqiang on 14/10/28.
+//  Copyright (c) 2014年 王焕强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RememberRoadTests/RememberRoadTests.swift
@@ -0,0 +1,36 @@
+//
+//  RememberRoadTests.swift
+//  RememberRoadTests
+//
+//  Created by wanghuanqiang on 14/10/28.
+//  Copyright (c) 2014年 王焕强. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RememberRoadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
