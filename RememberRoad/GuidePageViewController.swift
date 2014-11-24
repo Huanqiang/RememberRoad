0c1511d4eee9c5558a5546d21dfc521f137e3c6f
861645007 <861645007@qq.com>
2014-11-24 14:57:43 +0800

    完成引导页面

--- RememberRoad/GuidePageViewController.swift
+++ RememberRoad/GuidePageViewController.swift
@@ -21,14 +21,13 @@ class GuidePageViewController: UIViewController, MYIntroductionDelegate {
     }
     
     func GuideInterfaceOperation() {
-        self.createGuidePage()
-//        var str: String = LogInToolClass.shareInstance().getInfo("firstLogin")
-//        if str == "1" {
-//            self.gotoMainViewController()
-//        }else {
-//            LogInToolClass.shareInstance().saveInfo("1", infoType: "firstLogin")
-//            self.createGuidePage()
-//        }
+        var str: String = LogInToolClass.shareInstance().getInfo("firstLogin")
+        if str == "1" {
+            self.gotoMainViewController()
+        }else {
+            LogInToolClass.shareInstance().saveInfo("1", infoType: "firstLogin")
+            self.createGuidePage()
+        }
     }
     
     func createGuidePage() {
